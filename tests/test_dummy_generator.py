68f6ba3dabc59e08d378583bf72de9751b9485dc
EltonLCC <lccechai@gmail.com>
2025-07-16 00:02:43 +0000

    [feat] testcase for firm state dummy data

---
+++ tests/test_dummy_generator.py
@@ -0,0 +1,79 @@
+import unittest
+
+import numpy as np
+import tensorflow as tf
+
+from data_models.firm_state import FirmState
+from data_models.flow_variables import FlowVariables
+from synthetic_generator.dummy_generator import dummy_generator
+
+
+class TestDummyGenerator(tf.test.TestCase):
+    """Tests for dummy_generator"""
+
+    def test_invalid_num_firms_raises_error(self):
+        """
+        Tests that the function raises an AssertionError if num_firms is not positive.
+        """
+        with self.assertRaisesRegex(
+            AssertionError, "Number of firms must be greater than 0"
+        ):
+            dummy_generator(num_firms=0)
+
+        with self.assertRaisesRegex(
+            AssertionError, "Number of firms must be greater than 0"
+        ):
+            dummy_generator(num_firms=-5)
+
+    def test_accounting_principles(self):
+        """
+        Tests that the accounting principles are correctly applied in the dummy generator.
+        The value shuold be same as the total amount in table 25 (page 255, 1999 yr column).
+        """
+        firm_state, _ = dummy_generator(num_firms=1)
+
+        # Calculate total assets
+        total_assets = (
+            firm_state.CA[0][0]
+            + firm_state.MA[0][0]
+            + firm_state.BU[0][0]
+            + firm_state.OFA[0][0]
+        )
+
+        # Calculate total liabilities and equity
+        total_liabilities_equity = (
+            firm_state.CL[0][0]
+            + firm_state.LL[0][0]
+            + firm_state.ASD[0][0]
+            + firm_state.OUR[0][0]
+            + firm_state.SC[0][0]
+            + firm_state.RR[0][0]
+            + firm_state.URE[0][0]
+            + firm_state.PFt_0[0][0]
+            + firm_state.PFt_1[0][0]
+            + firm_state.PFt_2[0][0]
+            + firm_state.PFt_3[0][0]
+            + firm_state.PFt_4[0][0]
+            + firm_state.PFt_5[0][0]
+        )
+
+        self.assertAllClose(
+            total_assets,
+            tf.constant(6236489.0),
+            msg="Total assets do not equal to the number at table 25 (1999 yr).",
+        )
+
+        self.assertEqual(
+            total_liabilities_equity,
+            tf.constant(6236485.0),
+            msg="Total liabilities do not equal to the number at table 25 (1999 yr).",
+        )
+
+        print(
+            f"Total Assets: {total_assets}, Total Liabilities and Equity: {total_liabilities_equity}"
+        )
+
+
+# This allows the test to be run from the command line
+if __name__ == "__main__":
+    unittest.main()
